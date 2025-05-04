00547cfbda3335b0757678d0ee4f02673fb4a70f
Selim Sheta <selim.sheta98@hotmail.com>
2025-05-04 21:53:04 +0100

    Better layout for about page

--- my-website/src/app/about/page.tsx
+++ my-website/src/app/about/page.tsx
@@ -22,6 +22,7 @@ export default function Page() {
                 <Header />
                 {/* Hero Section */}
                 <section className="main-section">
+                    {/* Main content */}
                     <div className="split-layout">
                         <div className="split-text">
                             <p className="text-body">
@@ -36,22 +37,11 @@ export default function Page() {
                             <p className="text-body">
                                 My main focus is on designing, implementing, benchmarking, and refining real-time audio algorithms. 
                                 Whether you're facing a technical challenge or exploring new ideas, I can help:<br/>
-                                <span className="inline-block ml-4">• Review the latest research and industry standards</span><br/>
-                                <span className="inline-block ml-4">• Prototype, test, and iterate</span><br/>
-                                <span className="inline-block ml-4">• Develop tangible solutions, incorporate feedback</span><br/>
-                                <span className="inline-block ml-4">• Assess audio quality and performance</span><br/>
-                                <span className="inline-block ml-4">• Provide reports, specifications and unit test data</span>
-                            </p>
-                            <p className="text-body">
-                                I have industrial experience in audio synthesis, creative effects, binaural audio, frequency-domain processing, 
-                                artificial reverberation, dynamic range effects, and more. 
-                            </p>
-                            <p className="text-body">
-                                My main tools include C++ (JUCE, MAX MSP, Unreal Engine), Matlab, JavaScript (Frontend, Web Audio API), C# (Unity), and Python — and I'm always learning more. 
-                            </p>
-                            <p className="text-body">
-                                I'm fluent in English and French, and I'm currently learning Spanish.<br/>
-                                Finally, I'm addicted to Chess. <a className="link-style" href="https://link.chess.com/friend/5SGeOR" target="_blank" rel="noopener noreferrer">Add me on chess.com!</a>
+                                <span className="inline-block ml-4">→ Review the latest research and industry standards</span><br/>
+                                <span className="inline-block ml-4">→ Prototype, test, and iterate</span><br/>
+                                <span className="inline-block ml-4">→ Develop tangible solutions, incorporate feedback</span><br/>
+                                <span className="inline-block ml-4">→ Assess audio quality and performance</span><br/>
+                                <span className="inline-block ml-4">→ Provide reports, specifications and unit test data</span>
                             </p>
                         </div>
                         <div className='split-image'>
@@ -61,7 +51,36 @@ export default function Page() {
                                 buttonHref="/contact"
                                 description="Digital Environment Research Institute launch at Queen Mary (March 2022)"
                             />
-                            <p className="text-xl mt-8 mb-4">Latest Post</p>
+                        </div>
+                    </div>
+                    {/* Supporting content */}
+                    <div className="split-layout-reverse">
+                        <div className="split-text">
+                            <div className="w-full space-y-4 text-left text-base lg:text-xl">
+                                <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
+                                    <div className="font-semibold col-span-1">Technologies</div>
+                                    <div className="col-span-1 lg:col-span-3">
+                                    C++ (JUCE, Max MSP, Unreal Engine), Matlab, JavaScript (Frontend, Web Audio API), C# (Unity), and Python
+                                    </div>
+
+                                    <div className="font-semibold col-span-1">Languages</div>
+                                    <div className="col-span-1 lg:col-span-3">
+                                        Fluent in English and French, currently learning Spanish
+                                    </div>
+
+                                    <div className="font-semibold col-span-1">Experience</div>
+                                    <div className="col-span-1 lg:col-span-3">
+                                        Audio synthesis, creative effects, binaural audio, frequency-domain processing, 
+                                        artificial reverberation, dynamic range effects, etc.
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="mt-4 lg:text-xl text-left">
+                            <br></br>
+                                Also, I'm addicted to Chess. <a className="link-style" href="https://link.chess.com/friend/5SGeOR" target="_blank" rel="noopener noreferrer">Add me on chess.com!</a>
+                            </p>
+                        </div>
+                        <div className='split-image'>
                             <div className="w-full aspect-video">
                                 <iframe
                                     src="https://www.youtube.com/embed/c_93Iqw4kmk?si=xhiP30Fv2cPcTSaT"
